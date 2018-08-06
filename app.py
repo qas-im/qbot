775f818643310380d804c4cecab58961d844a5fb
Qasim Shareh <qshareh@uci.edu>
2018-08-06 19:24:42 -0400

    clicks second link and catches errors

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome("/usr/local/bin/chromedriver")
-driver.get("http://www.google.com/")
-time.sleep(5)
+driver.get("https://www.google.com/")
+time.sleep(2)
+search_box = driver.find_element_by_name("q")
+search_box.send_keys("qasim shareh")
+time.sleep(2)
+search_box.send_keys(Keys.RETURN)
+time.sleep(2)
+try:
+    link = driver.find_element_by_xpath('(//h3)[2]/a')
+    link.click()
+except NoSuchElementException:
+    print("failed to find link")
+driver.Close()
