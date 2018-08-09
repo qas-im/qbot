5a03c6b34bca72e44000cbb3bc88f18ede9f3235
Qasim Shareh <qshareh@uci.edu>
2018-08-08 23:48:35 -0400

    wait to click links based on href text

--- app.py
+++ app.py
@@ -12,14 +12,17 @@ driver.get("https://thechinatownmarket.com/")               # loading up chinato
 wait = WebDriverWait(driver, 20)                            # wait used for timing out loading
 
 
-current_time = datetime.now()                               # getting current time
-while current_time.hour != 11:                              # wait for time to hit exactly 11:00
+"""current_time = datetime.now()                               # getting current time
+while current_time.second != 15:                              # wait for time to hit exactly 11:00
     driver.refresh()                                        # refresh page
-    current_time = datetime.now()                           # get current time again
+    current_time = datetime.now()"""                           # get current time again
 start_time = time()                                         # get start time once bot moves through site
 
+# waits for item to load and clicks item
+link = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[href*='hat']")))
+link.click()
 
-
+# close driver when done and print time
 driver.close()                                              # closes driver at the end
 print("~~~~~ Finished in {0:.2f} seconds ~~~~~"             # print out time elapsed
       .format(time() - start_time))                         # subtract current time from starting time
