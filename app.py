c2dae5c1814ad2d5cf6eff8fad3524f27a232259
Qasim Shareh <qshareh@uci.edu>
2018-08-08 23:18:06 -0400

    restarted website search and added elapsed time

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
-import time
+from time import sleep,time
 from datetime import datetime
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-driver = webdriver.Chrome("/usr/local/bin/chromedriver")
-driver.get("https://www.google.com/")
+driver = webdriver.Chrome("/usr/local/bin/chromedriver")    # starting chrome driver
+driver.get("https://thechinatownmarket.com/")               # loading up chinatown website
+wait = WebDriverWait(driver, 20)                            # wait used for timing out loading
 
-current_time = datetime.now()
-while current_time.hour != 11:     # wait for time to hit exactly 11:00
-    driver.refresh()
-    current_time = datetime.now()
 
-time.sleep(2)
-search_box = driver.find_element_by_name("q")
-search_box.send_keys("qasim shareh")
-time.sleep(2)
-search_box.send_keys(Keys.RETURN)
-time.sleep(2)
-try:
-    link = driver.find_element_by_xpath('(//h3)[2]/a')
-    link.click()
-except NoSuchElementException:
-    print("failed to find link")
+current_time = datetime.now()                               # getting current time
+while current_time.hour != 11:                              # wait for time to hit exactly 11:00
+    driver.refresh()                                        # refresh page
+    current_time = datetime.now()                           # get current time again
+start_time = time()                                         # get start time once bot moves through site
 
-driver.close()
+
+
+driver.close()                                              # closes driver at the end
+print("~~~~~ Finished in {0:.2f} seconds ~~~~~"             # print out time elapsed
+      .format(time() - start_time))                         # subtract current time from starting time
