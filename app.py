8c7e5b6af9be79a653bde199638f61e20cc24e37
Qasim Shareh <qshareh@uci.edu>
2018-08-15 22:01:08 -0400

    loops through clothing list and uses config file to enter shipping/billing

--- app.py
+++ app.py
@@ -34,10 +34,9 @@ while current_time.minute != 59 or current_time.second != 40:
     current_time = datetime.now()"""
 
 # temporary sleep for testing
-sleep(30)
+#sleep(5)
 
 # loads website and waits for a certain time before starting
-driver.get("https://thechinatownmarket.com/")
 """current_time = datetime.now()                               # getting current time
 while current_time.hour != 11:                              # wait for time to hit exactly 11:00
     driver.refresh()                                        # refresh page
@@ -45,17 +44,21 @@ while current_time.hour != 11:                              # wait for time to h
 start_time = time()                                         # get start time once bot moves through site
 
 
-# waits for item to load and clicks item
-link = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href*="ctm-l-s"]')))
-link.click()
-
-
-# waits for dropdown to load, selects size, and adds to cart
-dropdown = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="SingleOptionSelector-0"]')))
-select = Select(dropdown)
-select.select_by_visible_text("LARGE")
-driver.find_element_by_xpath('//*[@id="shopify-section-product"]/div/div/div[2]/form/div/button/span').click()
-
+# iterates through clothing list in info dict; separates all variables by name, size, and category
+for name,[size,category] in info["clothing"].items():
+    # first goes to category page, then finds clothing with name in link and clicks it
+    driver.get("https://thechinatownmarket.com/collections/"+category)
+    link = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href*="{}"]'.format(name))))
+    link.click()
+    # checks to see if size selection is necessary, waits for dropdown to load, selects size, and adds to cart
+    if size != "OS":
+        dropdown = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="SingleOptionSelector-0"]')))
+        select = Select(dropdown)
+        select.select_by_visible_text(size)
+        driver.find_element_by_xpath('//*[@id="shopify-section-product"]/div/div/div[2]/form/div/button/span').click()
+    else:
+        add_to_cart = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="shopify-section-product"]/div/div[1]/div[2]/form/button')))
+        add_to_cart.click()
 
 # waits for checkout button to load and clicks it
 checkout = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="MainContent"]/div/div/form/div[3]/input[2]')))
@@ -63,15 +66,31 @@ checkout.click()
 
 
 # waits for first box to load, enters information, and clicks to shipping info
-info = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="checkout_email"]')))
-info.send_keys("johndoe@gmail.com" + Keys.TAB + Keys.TAB + \
-               "John" + Keys.TAB + \
-               "Doe" + Keys.TAB + \
-               "1234 California St" + Keys.TAB + Keys.TAB + \
-               "San Ramon" + Keys.TAB + Keys.TAB + \
-               "C" + Keys.TAB + \
-               "94582")
+shipping = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="checkout_email"]')))
+shipping.send_keys(info["google"]["email"] + Keys.TAB + Keys.TAB + \
+               info["shipping"]["first"] + Keys.TAB + \
+               info["shipping"]["last"] + Keys.TAB + \
+               info["shipping"]["street"] + Keys.TAB + Keys.TAB + \
+               info["shipping"]["city"] + Keys.TAB + Keys.TAB + \
+               info["shipping"]["state"] + Keys.TAB + \
+               info["shipping"]["zip"])
 driver.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/form/div[2]/button').click()
+
+
+# waits for continue to shipping button to load and clicks it
+continue_button = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div[1]/div[2]/div/form/div[2]/button')))
+continue_button.click()
+
+
+# waits for payment info to load, enters information and clicks submit
+payment = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="main-header"]')))
+payment.send_keys(Keys.TAB + Keys.TAB + \
+                    info["billing"]["number"] + Keys.TAB + \
+                    info["shipping"]["first"] + " " + info["shipping"]["last"] + Keys.TAB + \
+                    info["billing"]["expiration"] + Keys.TAB + \
+                    info["billing"]["cvv"])
+submit = driver.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[2]/div/div/form/div[4]/div[1]/button')
+#submit.click() # uncomment to actually make purchase
 
 
 # close driver when done and print time
