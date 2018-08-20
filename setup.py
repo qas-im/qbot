06b0815c3c955d0013f276318d7e2d4f613b584f
Qasim Shareh <qshareh@uci.edu>
2018-08-20 12:31:45 -0400

    updated setup to have phone number and colour index

--- setup.py
+++ setup.py
@@ -2,137 +2,139 @@ import json
 
 
 def gather_info():
-    """
-    function responsible for making a dict of all desired clothes
-    repeatedly asks for user input until user specifies they are done
-    asks for shipping info
-    writes dict to json file called config.json
-    """
-    # master dictionary
-    info = {}
+	"""
+	function responsible for making a dict of all desired clothes
+	repeatedly asks for user input until user specifies they are done
+	asks for shipping info
+	writes dict to json file called config.json
+	"""
+	# master dictionary
+	info = {}
 
-    # while loop that gathers clothing dictionary until user is satisfied with dictionary
-    confirmation = "no"
-    while confirmation != "yes":
-        google = google_info()
-        confirmation = raw_input("\nIs the info you entered correct? (yes to continue/any key to restart): ")
-    info["google"] = google
+	# while loop that gathers clothing dictionary until user is satisfied with dictionary
+	confirmation = "no"
+	while confirmation != "yes":
+		google = google_info()
+		confirmation = raw_input("\nIs the info you entered correct? (yes to continue/any key to restart): ")
+	info["google"] = google
 
-    # while loop that gathers clothing dictionary until user is satisfied with dictionary
-    confirmation = "no"
-    while confirmation != "yes":
-        clothing = clothing_info()
-        confirmation = raw_input("\nIs the info you entered correct? (yes to continue/any key to restart): ")
-    info["clothing"] = clothing
+	# while loop that gathers clothing dictionary until user is satisfied with dictionary
+	confirmation = "no"
+	while confirmation != "yes":
+		clothing = clothing_info()
+		confirmation = raw_input("\nIs the info you entered correct? (yes to continue/any key to restart): ")
+	info["clothing"] = clothing
 
-    # while loop that gathers shipping dictionary until user is satisfied with dictionary
-    confirmation = "no"
-    while confirmation != "yes":
-        shipping = shipping_info()
-        confirmation = raw_input("\nIs the info you entered correct? (yes to continue/any key to restart): ")
-    info["shipping"] = shipping
+	# while loop that gathers shipping dictionary until user is satisfied with dictionary
+	confirmation = "no"
+	while confirmation != "yes":
+		shipping = shipping_info()
+		confirmation = raw_input("\nIs the info you entered correct? (yes to continue/any key to restart): ")
+	info["shipping"] = shipping
 
-    # while loop that gathers billing dictionary until user is satisfied with dictionary
-    confirmation = "no"
-    while confirmation != "yes":
-        billing = billing_info()
-        confirmation = raw_input("\nIs the info you entered correct? (yes to continue/any key to restart): ")
-    info["billing"] = billing
+	# while loop that gathers billing dictionary until user is satisfied with dictionary
+	confirmation = "no"
+	while confirmation != "yes":
+		billing = billing_info()
+		confirmation = raw_input("\nIs the info you entered correct? (yes to continue/any key to restart): ")
+	info["billing"] = billing
 
-    return info
+	return info
 
 
 def clothing_info():
-    """
-    function responsible for gathering clothing info
-    clothes are keys while sizes are values
-    """
-    # dictionary for storing all clothing info and string for controlling while loop
-    clothing = {}
-    keep_adding = "yes"
+	"""
+	function responsible for gathering clothing info
+	clothes are keys while sizes are values
+	"""
+	# dictionary for storing all clothing info and string for controlling while loop
+	clothing = {}
+	keep_adding = "yes"
 
-    # while loop repeatedly asks user for name and size until user says no 
-    while keep_adding != "no":
-        name = raw_input("\nPlease enter name of clothing (or partial name): ")
-        size = raw_input("\nPlease enter size of clothing (OS for one size): ")
-        category = raw_input("\nPlease enter category of clothing: ")
-		url = raw_input("\nPlease enter url of clothing (or None if no url):")
-        clothing[name] = [size,category,url]
-        keep_adding = raw_input("\nAdd another? (no to finish/any key to continue): ")
+	# while loop repeatedly asks user for name and size until user says no 
+	while keep_adding != "no":
+		name = raw_input("\nPlease enter name of clothing (or partial name): ")
+		size = raw_input("\nPlease enter size of clothing (OS for one size): ")
+		category = raw_input("\nPlease enter category of clothing: ")
+		index = raw_input("\nPlease enter colour index of clothing: ")
+		clothing[name] = [size,category,index]
+		keep_adding = raw_input("\nAdd another? (no to finish/any key to continue): ")
 
-    # returns dict
-    return clothing
+	# returns dict
+	return clothing
 
 
 def shipping_info():
-    """
-    function responsible for gathering shipping information
-    """
-    # dictionary storing all shipping info
-    shipping = {}
-    
-    # asking user for information
-    name = raw_input("\nPlease enter first and last name: ")
-    street = raw_input("\nPlease enter street address: ")
-    city = raw_input("\nPlease enter city: ")
-    state = raw_input("\nPlease enter state: ")
-    zip_code = raw_input("\nPlease enter zip code: ")
+	"""
+	function responsible for gathering shipping information
+	"""
+	# dictionary storing all shipping info
+	shipping = {}
+	
+	# asking user for information
+	name = raw_input("\nPlease enter first and last name: ")
+	phone = raw_input("\nPlease enter phone number: ")
+	street = raw_input("\nPlease enter street address: ")
+	city = raw_input("\nPlease enter city: ")
+	state = raw_input("\nPlease enter state: ")
+	zip_code = raw_input("\nPlease enter zip code: ")
 
-    # adding information to dict
-    shipping["name"] = name
-    shipping["street"] = street
-    shipping["city"] = city
-    shipping["state"] = state
-    shipping["zip"] = zip_code
+	# adding information to dict
+	shipping["name"] = name
+	shipping["phone"] = phone
+	shipping["street"] = street
+	shipping["city"] = city
+	shipping["state"] = state
+	shipping["zip"] = zip_code
 
-    # returns dict
-    return shipping
+	# returns dict
+	return shipping
 
 
 def billing_info():
-    """
-    function responsible for gathering billing information
-    """
-    # dictionary storing all billing info
-    billing = {}
+	"""
+	function responsible for gathering billing information
+	"""
+	# dictionary storing all billing info
+	billing = {}
 
-    # asking user for information
-    card_number = raw_input("\nPlease enter your credit card number: ")
-    expiration_month = raw_input("\nPlease enter your credit card's expiration_month date: ")
-	expiration_year = raw_input("\nPlease enter your credit card's expiration_year date: ")
-    cvv = raw_input("\nPlease enter your credit card's cvv: ")
+	# asking user for information
+	card_number = raw_input("\nPlease enter your credit card number: ")
+	expiration_month = raw_input("\nPlease enter your credit card's expiration month (MM): ")
+	expiration_year = raw_input("\nPlease enter your credit card's expiration year (YYYY): ")
+	cvv = raw_input("\nPlease enter your credit card's cvv: ")
 
-    # adding information to dict
-    billing["number"] = card_number
-    billing["expiration_month"] = expiration_month
+	# adding information to dict
+	billing["number"] = card_number
+	billing["expiration_month"] = expiration_month
 	billing["expiration_year"] = expiration_year
-    billing["cvv"] = cvv
+	billing["cvv"] = cvv
 
-    # returns dict
-    return billing
+	# returns dict
+	return billing
 
 
 def google_info():
-    """
-    function responsible for gathering google sign in information
-    """
-    # dictionary storing all google sign in info
-    google = {}
+	"""
+	function responsible for gathering google sign in information
+	"""
+	# dictionary storing all google sign in info
+	google = {}
 
-    # asking user for information
-    email = raw_input("\nPlease enter your email address: ")
-    password = raw_input("\nPlease enter your password: ")
+	# asking user for information
+	email = raw_input("\nPlease enter your email address: ")
+	password = raw_input("\nPlease enter your password: ")
 
-    # adding information to dict
-    google["email"] = email
-    google["password"] = password
+	# adding information to dict
+	google["email"] = email
+	google["password"] = password
 
-    # returns dict
-    return google
+	# returns dict
+	return google
 
 
 if __name__ == "__main__":
-    file_path = "./config.json"     # file path set to current working directory
-    data = gather_info()            # calls gather_info() to construct clothing dict
-    with open(file_path,"w") as f:  # opens json file to be written to
-        json.dump(data,f)           # writes data to json file
+	file_path = "./config.json"     # file path set to current working directory
+	data = gather_info()            # calls gather_info() to construct clothing dict
+	with open(file_path,"w") as f:  # opens json file to be written to
+		json.dump(data,f)           # writes data to json file
